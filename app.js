ec1aa893ada21cf6c8bc8674fc968879848176a9
srijani07 <srijaniparui07@gmail.com>
2021-07-21 20:48:06 +0530

    added leap year condition

---
+++ app.js
@@ -0,0 +1,15 @@
+var btnCheck = document.querySelector('#btn-check')
+var outputText = document.querySelector('#output')
+var year = document.querySelector('#input-text');
+
+function clickHandler() {
+    if((year.value % 400 == 0) || (year.value % 4 == 0 && year.value % 100 != 0)) {
+        outputText.innerHTML= "It is a leap year";
+        }
+    else
+     outputText.innerHTML = "It is not a leap year";
+}
+
+
+
+btnCheck.addEventListener("click", clickHandler);
