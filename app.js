478d0ee058986af22e624281f14a2070dc45289d
srijani07 <srijaniparui07@gmail.com>
2021-07-30 23:43:49 +0530

    styling : added css

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var outputText = document.querySelector('#output')
 var year = document.querySelector('#input-text');
 
 function clickHandler() {
+
+
+    if(isNaN(year.value) === true || year.value === "") {
+        outputText.innerHTML = "Please enter a valid input."
+    }
+
+    else {
     if((year.value % 400 == 0) || (year.value % 4 == 0 && year.value % 100 != 0)) {
         outputText.innerHTML= "It is a leap year";
         }
     else
      outputText.innerHTML = "It is not a leap year";
+}
 }
 
 
